4bdd9eba2273e74c498b80ea76eaef667390313a
Pedro Gutiérrez <pedro.gutierrez@form3.tech>
2020-09-18 18:26:19 +0200

    fix bugs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 REPO_OWNER=`echo $GITHUB_REPOSITORY | cut -d'/' -f1`
 REPO_NAME=`echo $GITHUB_REPOSITORY | cut -d'/' -f2`
-REPO_SLUG=$REPO_SLUG/$REPO_NAME
+REPO_SLUG=$REPO_OWNER/$REPO_NAME
 REPO_URL="https://github.com/$REPO_SLUG"
 COMMIT_URL="$REPO_URL/commit/$GITHUB_SHA"
 
@@ -27,7 +27,7 @@ case $INPUT_STATUS in
         ;;
 esac
 
-TEXT="The workflow `$GITHUB_WORKFLOW` has $TEXT_SUFFIX"
+TEXT='The workflow \`$GITHUB_WORKFLOW\` has $TEXT_SUFFIX'
 
 
 echo "color=$COLOR"
@@ -36,5 +36,4 @@ echo "repo slug=$REPO_SLUG"
 echo "commit url=$COMMIT_URL"
 
 
-curl -X POST -H "content-type: application/json" $INPUT_SLACK_URL \
-    '{ attachments": [ { "color": "$COLOR", "text": "$TEXT", "fields": [{ "title": "Repository", "short": true, "value": "<$REPO_URL|$REPO_SLUG>" }, { "title": "Ref", "short": true, "value": "$GITHUB_REF" }], "actions": [ {"type": "button", "text": "Commit", "url": "$COMMIT_URL"}, { "type": "button", "text": "Action", "url": "$COMMIT_URL/checks" }]}]}'
+curl -X POST -H "content-type: application/json" $INPUT_SLACK_URL -d '{ attachments": [ { "color": "$COLOR", "text": "$TEXT", "fields": [{ "title": "Repository", "short": true, "value": "<$REPO_URL|$REPO_SLUG>" }, { "title": "Ref", "short": true, "value": "$GITHUB_REF" }], "actions": [ {"type": "button", "text": "Commit", "url": "$COMMIT_URL"}, { "type": "button", "text": "Action", "url": "$COMMIT_URL/checks" }]}]}'
